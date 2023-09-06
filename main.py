03b3969b88fbc7006e9bef20c0a750907e1a4dac
Antonio Sousa <antonioufpi@gmail.com>
2023-09-06 19:59:51 -0300

    estrutura do usuário

---
+++ main.py
@@ -0,0 +1,10 @@
+base_de_dados_do_usuario = {}
+
+nome_do_usuario = input("Digite seu nome: ")
+senha_do_usuario = input("Digite sua senha: ")
+
+base_de_dados_do_usuario[nome_do_usuario] = senha_do_usuario
+
+
+
+
