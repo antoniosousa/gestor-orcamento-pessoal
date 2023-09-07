8dc7fc084021252abd5609cac9530445d97e801e
Antonio Sousa <antonioufpi@gmail.com>
2023-09-07 17:35:07 -0300

    inserido o uso do dicionário para guardar os valores de usuário e de senha

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
-base_de_dados_do_usuario = {}
+# aqui criamos nossa estrutura de dados chamada de dicionário
+# funcionará como um banco de dados, mas ao pé da letra não é!
+banco_usuario = {}
 
-nome_do_usuario = input("Digite seu nome: ")
-senha_do_usuario = input("Digite sua senha: ")
+# aqui será o trecho de cadastro de usuarios de nosso sistema
+# solicita o cadastro do nome
+nome_do_usuario = input("Nome do usuário: ")
+# salva o nome no dicionário
+banco_usuario["nome"] = nome_do_usuario
+# solicita o cadastro da senha
+senha_do_usuario = input("Senha do usuário: ")
+# salva a senha no dicionáro
+banco_usuario["senha"] = senha_do_usuario
 
-base_de_dados_do_usuario[nome_do_usuario] = senha_do_usuario
+# aqui já é a parte do sistema que pergunta o usuário e a senha para depois verificarmos se estão corretos
+nome_do_usuario_informado = input("Nome: ")
+senha_do_usuario_informado = input("Senha: ")
 
-
-
-
+# aqui é a verificação do que o usuário informou, nome e senha.
+if banco_usuario["nome"] == nome_do_usuario_informado and banco_usuario["senha"] == senha_do_usuario_informado:
+    print("Login realizado com sucesso!")
+else:
+    print("Usuário ou senha incorretos! Favor verificar e tentar novamente.")
